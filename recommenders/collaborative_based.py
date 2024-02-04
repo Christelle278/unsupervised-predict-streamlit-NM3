b0fca7095f345ea7c94991abde13efd8fe979e6a
Christelle278 <142970095+Christelle278@users.noreply.github.com>
2024-02-04 17:44:04 +0200

    Add files via upload

--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -130,34 +130,66 @@ def collab_model(movie_list,top_n=10):
 
     indices = pd.Series(movies_df['title'])
     movie_ids = pred_movies(movie_list)
+
+    print(f'movies {movie_ids}')
+
     df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
     for i in movie_ids :
         #df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
         df_init_users=pd.concat([df_init_users, ratings_df[ratings_df['userId']==i]])
+
+    
+    print(f'init users {df_init_users}')
+
     # Getting the cosine similarity matrix
     cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
+
+    
+    print(f'cosine sim {cosine_sim}')
+
     idx_1 = indices[indices == movie_list[0]].index[0]
     idx_2 = indices[indices == movie_list[1]].index[0]
     idx_3 = indices[indices == movie_list[2]].index[0]
     # Creating a Series with the similarity scores in descending order
 
-    print(type(idx_1))
-    
+    print(f'idx 1 {idx_1}')
+    print(f'idx 2 {idx_2}')
+    print(f'idx 3 {idx_3}')
+
     rank_1 = cosine_sim[idx_1]
     rank_2 = cosine_sim[idx_2]
     rank_3 = cosine_sim[idx_3]
+
+    print(f'rank 1 {rank_1}')
+    print(f'rank 2 {rank_2}')
+    print(f'rank 3 {rank_3}')
+
     # Calculating the scores
     score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+
+    print(f'score 1 {score_series_1}')
+    print(f'score 2 {score_series_2}')
+    print(f'score 3 {score_series_3}')
+
      # Appending the names of movies
     #listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
     listings = pd.concat([score_series_1, score_series_2, score_series_3])
+
+    print(f'listings {listings}')
+
     recommended_movies = []
     # Choose top 50
     top_50_indexes = list(listings.iloc[1:50].index)
+
+    print(f'top 50 indexes {top_50_indexes}')
+
     # Removing chosen movies
     top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+
+    print(f'top indexes {top_indexes}')
+
     for i in top_indexes[:top_n]:
         recommended_movies.append(list(movies_df['title'])[i])
     return recommended_movies
